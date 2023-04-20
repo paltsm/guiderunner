8f6fc04c268ad529c345c70f1ebda3b83fa3cef6
misi <misipalotas@gmail.com>
2023-04-20 15:49:53 +0200

    username and email are now unique

--- Guiderunner_Backend/src/typeorm/entities/account.entity.ts
+++ Guiderunner_Backend/src/typeorm/entities/account.entity.ts
@@ -5,10 +5,10 @@ export class Account{
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({unique: true})
     username: string;
 
-    @Column()
+    @Column({unique: true})
     email: string;
 
     @Column()
